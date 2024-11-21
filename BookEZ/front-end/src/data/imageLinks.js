e2f7033a1002dc96896d1a4270fa2a7b9a5e9c60
Chengtao <chengtaolinctl@gmail.com>
2024-11-21 14:46:48 -0500

    transfer bookez client to new repo [here](https://github.com/TaoLyn838/BookEZ-Client-Deploy)

--- BookEZ/front-end/src/data/imageLinks.js
+++
@@ -1,5 +0,0 @@
-export const imageLinks = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVw62NkPwHoYfvjy-Ai_y31HFMwDRUHOIacQ&s",
-    "https://salonbizsoftware.com/wp-content/uploads/2021/05/Salon-Decor-scaled.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk6V62I7vEvLkK6CtLLNfQw6s1O--gQVt42g&s"
-]
